207b7b733903f269dd6a75761342e0d13a6e6aa8
Serena Kuo <59319273+serenackuo@users.noreply.github.com>
2020-08-30 20:21:12 -0400

    Add Initial Commit Files via Upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          900: '#1d2226',
+          700: '#3e4952',
+        },
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+        mintGreen: '#6cbba9',
+      },
+      fontFamily: {
+        body: ['PT Sans'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
